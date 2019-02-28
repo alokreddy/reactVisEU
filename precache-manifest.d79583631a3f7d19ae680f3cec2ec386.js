5940e0d85f56b82d2519b011f98ab0955231460a
Katanguri Tarun Alok Reddy <Alok@hyd.swtpl.local>
2019-02-28 20:18:11 +0530

    Updates

---
+++ precache-manifest.d79583631a3f7d19ae680f3cec2ec386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "20c9b5cab85c0e1ef86a",
+    "url": "/reactVisEU/static/js/runtime~main.20c9b5ca.js"
+  },
+  {
+    "revision": "d6bb0297b8367dc05c5a",
+    "url": "/reactVisEU/static/js/main.d6bb0297.chunk.js"
+  },
+  {
+    "revision": "c28345c7aa4ca2d33c73",
+    "url": "/reactVisEU/static/js/2.c28345c7.chunk.js"
+  },
+  {
+    "revision": "d6bb0297b8367dc05c5a",
+    "url": "/reactVisEU/static/css/main.49bca369.chunk.css"
+  },
+  {
+    "revision": "c28345c7aa4ca2d33c73",
+    "url": "/reactVisEU/static/css/2.60d12c10.chunk.css"
+  },
+  {
+    "revision": "733f5c17b4352725c6f9aafff11fda36",
+    "url": "/reactVisEU/index.html"
+  }
+];
